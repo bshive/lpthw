33e156b4714a237e05ed9a686f1272a5e348bcf7
ShiveIT <bradwshive@yahoo.com>
2015-09-11 23:38:28 -0500

    first commit

---
+++ ex9.py
@@ -0,0 +1,10 @@
+# ex9.py
+
+age = raw_input ("How old are you? ")
+height = raw_input ("How tall are you?")
+weight = raw_input ("How mUch do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight)
+	
+	
